3ca3401e43efad2e19ecd3351c85733b873c4f26
nyx-desdev <nyx.desdev@gmail.com>
2022-09-24 19:21:52 +0530

    promises to async

--- src/index.js
+++ src/index.js
@@ -1,31 +1,44 @@
 #!/usr/bin/env node
 import inquirer from "inquirer";
-import { writeFileSync, readFile, readFileSync } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { writeFile, readdir, readFile } from "fs/promises";
 
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-    {
-      type: "list",
-      message: "Pick the framework to generate a gitignore file",
-      name: "framework",
-      choices: ["react", "nextjs"],
-    },
-  ])
-  .then(({ framework }) => {
-    let fileToWrite = "";
-    if (framework === "react") {
-      fileToWrite = readFileSync("./src/config/react_gitignore.txt", "utf8");
-    } else if (framework === "nextjs") {
-      fileToWrite = readFileSync("./src/config/next_gitignore.txt", "utf8");
-    }
-    const cwd = process.cwd();
-    writeFileSync(cwd + "/.gitignore", fileToWrite);
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
+const configFiles = {};
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+const configFolderPath = path.resolve(__dirname, "config");
+const defaultTechnology = { technology: "react(Default)" };
+
+(async () => {
+  const files = await readdir(configFolderPath).catch(console.log);
+  const useDefault = process.argv[2] === "-y";
+
+  for (let i of files) {
+    const frameworkName = i.split(".")[0];
+    configFiles[frameworkName] = path.join(configFolderPath, i);
+  }
+
+  const { technology } = useDefault
+    ? defaultTechnology
+    : await inquirer.prompt([
+        {
+          type: "list",
+          message: "Pick the framework to generate a gitignore file:",
+          name: "technology",
+          choices: Object.keys(configFiles),
+        },
+      ]);
+
+  let config = await readFile(configFiles[technology]).catch(console.log);
+
+  const gitignore = path.join(process.cwd(), ".gitignore");
+
+  await writeFile(gitignore, config.toString()).catch((err) => {
+    console.log(err);
+    process.exit();
   });
+
+  console.log("gitignore successfully created");
+})();
